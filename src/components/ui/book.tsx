235114da87ba11ad4b71f8bc9703a45118ea5e30
RML1812 <raditya.lacavi@gmail.com>
2024-05-30 18:24:08 +0700

    adding every parameter for Book

--- src/components/ui/book.tsx
+++ src/components/ui/book.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Link from "next/link";
 import HTMLFlipBook from "react-pageflip";
 
 
 const Book: React.FC = () => {
-  const folderPath = "/book-content";
-  const numberOfPages = 32;
+  const folderPath = "/";
+  const numberOfPages = 8;
 
   const generateImages = () => {
     const images = [];
@@ -16,17 +15,51 @@ const Book: React.FC = () => {
     return images;
   };
 
+  const Style: React.CSSProperties = {
+    color: 'blue',
+    fontSize: '20px',
+    margin: '10px',
+    padding: '5px',
+    border: '1px solid black',
+    backgroundColor: 'lightgray',
+    textAlign: 'center',
+    borderRadius: '8px'
+  };
+
   return (
-    <Link className="mt-[70px]" href="/main">Main</Link>
-    // <HTMLFlipBook width={350} height={600}>
-    //   <div/>
-    //   {generateImages().map((img, index) => (
-    //     <div className="Page" key={index}>
-    //       {img}
-    //     </div>
-    //   ))}
-    //   <div/>
-    // </HTMLFlipBook>
+    <HTMLFlipBook 
+      width={350} 
+      height={600} 
+      className="demo-book" 
+      style={Style}
+      startPage={0} 
+      size={"fixed"} 
+      minWidth={0} 
+      maxWidth={350} 
+      minHeight={0} 
+      maxHeight={600} 
+      drawShadow={true} 
+      flippingTime={1000} 
+      usePortrait={true} 
+      startZIndex={0} 
+      autoSize={true} 
+      maxShadowOpacity={1} 
+      showCover={true} 
+      mobileScrollSupport={true} 
+      clickEventForward={true} 
+      useMouseEvents={true} 
+      swipeDistance={30} 
+      showPageCorners={true} 
+      disableFlipByClick={false}
+    >
+      <div/>
+      {generateImages().map((img, index) => (
+        <div className="Page" key={index}>
+          {img}
+        </div>
+      ))}
+      <div/>
+    </HTMLFlipBook>
   );
 };
 
