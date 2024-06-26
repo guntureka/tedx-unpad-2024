9b9977286a056b67cb9c486f639bb9236480492d
RML1812 <raditya.lacavi@gmail.com>
2024-06-26 10:28:18 +0700

    fix sizing issue, change youtube to vimeo

--- src/components/partnership/VideoCarousel.tsx
+++ src/components/partnership/VideoCarousel.tsx
@@ -31,22 +31,20 @@ const VideoCarousel: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
       {isLoading ? (
-        <Skeleton className="h-[calc(280px*9/16)] w-screen max-w-[280px] md:h-[calc(448px*9/16)] md:max-w-md lg:h-[calc(576px*9/16)] lg:max-w-xl" />
+        <Skeleton className="h-[calc(280px*9/16)] w-screen max-w-[280px] md:h-[calc(448px*9/16)] md:max-w-md lg:h-[calc(672px*9/16)] lg:max-w-2xl" />
       ) : (
         videos.length > 0 && (
-          <Carousel className="w-full max-w-[280px] md:max-w-md lg:max-w-xl">
+          <Carousel className="w-screen max-w-[280px] md:max-w-md lg:max-w-2xl">
             <CarouselContent>
               {videos.map((video) => (
                 <CarouselItem key={video.id} className="w-full">
-                  <iframe
-                    width="100%"
-                    height="100%"
-                    src={video.url}
-                    title={`YouTube video player ${video.id}`}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    className="aspect-video w-full"
-                  ></iframe>
+                  <iframe 
+                    width="100%" 
+                    height="100%" 
+                    src={`${video.url}?autopause=0&autoplay=1&byline=0&controls=0&dnt=1&loop=1&muted=1&portrait=0&title=0`} 
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                    className="aspect-video w-full"  
+                  />
                 </CarouselItem>
               ))}
             </CarouselContent>
