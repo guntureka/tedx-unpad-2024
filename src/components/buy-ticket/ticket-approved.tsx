6f81db40c0ebdf28c2f7705808c733a68b2100c6
sultanIlyasa <sultan21004@mail.unpad.ac.id>
2024-06-28 11:24:38 +0700

    add-payment-approved-button

--- src/components/buy-ticket/ticket-approved.tsx
+++ src/components/buy-ticket/ticket-approved.tsx
@@ -7,19 +7,24 @@ const TicketApproved = () => {
   const [isPending, startTransition] = useTransition();
   return (
     <main className="flex min-h-screen w-full flex-col px-10 py-40 lg:px-20">
-      <div className="md:px-13 flex w-full flex-col space-y-14 sm:px-10 xl:px-32">
-        <p className="text-center text-4xl">
-          Get your ticket here!
+      <div className="md:px-13 flex w-full flex-col sm:px-10 xl:px-32">
+        <p className="text-center text-4xl font-bold">Congratulations!</p>
+        <p className="text-center text-xl">
+          You have been selected as one of the 100 Wisdom Wonderers.
+        </p>
+        <p className="text-center text-xl mt-4">
+          To confirm your spot, please proceed with the payment by clicking
+          the button below
         </p>
         <div className="flex justify-center">
-          <Link href="/">
+          <Link href="https://forms.gle/kH6DXrTD6zzuXuvx8 ">
             <button
               type="button"
-              className={`rounded-lg bg-red-600 px-8 py-4 text-white duration-150 hover:bg-red-700 ${
+              className={`rounded-lg bg-red-600 px-8 py-4  mt-8 font-bold text-white duration-150 hover:bg-red-700 ${
                 isPending ? "cursor-progress opacity-50" : ""
               }`}
             >
-              Get Ticket!
+              PAY NOW
             </button>
           </Link>
         </div>
