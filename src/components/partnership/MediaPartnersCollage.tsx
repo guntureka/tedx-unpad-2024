0bb380d7f3792e9b15d95a2690a70f150a4ce6e5
RML1812 <raditya.lacavi@gmail.com>
2024-07-02 02:19:02 +0700

    fix:revision layouting, use img, removing prev json

--- src/components/partnership/MediaPartnersCollage.tsx
+++ src/components/partnership/MediaPartnersCollage.tsx
@@ -1,53 +1,33 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 
-interface MediaPartners {
-  id: string;
-  url: string;
-  size: "small" | "medium" | "large";
-}
-
-const MediaPartnersCollage: React.FC = () => {
-  const [MediaPartners, setMediaPartners] = useState<MediaPartners[]>([]);
+const MediaPartnersCollage = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchMediaPartners = async () => {
-      setIsLoading(true);
-      const response = await fetch("/partnership/media-partners.json");
-      const data = await response.json();
-      const sortedData = data.sort((a: MediaPartners, b: MediaPartners) => {
-        const sizeOrder = { large: 1, medium: 2, small: 3 };
-        return sizeOrder[a.size] - sizeOrder[b.size];
-      });
-      setMediaPartners(sortedData);
+    const img = new Image();
+    img.src = "partnership/medpart_2-7-2024.png";
+    img.onload = () => {
       setIsLoading(false);
     };
-    fetchMediaPartners();
   }, []);
 
   return (
-    <div className="flex w-full max-w-sm flex-wrap items-center justify-center md:max-w-md lg:max-w-xl">
+    <div className="flex w-full justify-center px-[50px] md:px-[100px] lg:px-[140px]">
       {isLoading ? (
-        <div className="flex flex-wrap items-center justify-center">
-          <Skeleton className="m-2 h-[100px] w-[100px]" />
-          <Skeleton className="m-2 h-[75px] w-[75px]" />
-          <Skeleton className="m-2 h-[50px] w-[50px]" />
+        <div className="flex w-full justify-center">
+          <div className="relative w-full" style={{ paddingTop: "19.41%" }}>
+            <Skeleton className="absolute left-0 top-0 h-full w-full" />
+          </div>
         </div>
       ) : (
-        MediaPartners.length > 0 &&
-        MediaPartners.map((mediapartner) => (
-          <div
-            key={mediapartner.id}
-            className={`m-2 flex items-center justify-center ${mediapartner.size === "large" ? "h-[100px]" : ""} ${mediapartner.size === "medium" ? "h-[75px]" : ""} ${mediapartner.size === "small" ? "h-[50px]" : ""} `}
-          >
-            <img
-              src={mediapartner.url}
-              alt={`MediaPartnership logo ${mediapartner.id}`}
-              className="h-full w-auto object-contain"
-            />
-          </div>
-        ))
+        <div className="flex w-full justify-center">
+          <img
+            src="partnership/medpart_2-7-2024.png"
+            className="w-full"
+            alt="Sponsor"
+          />
+        </div>
       )}
     </div>
   );
